0572a94e32d193691f6a38dc2b9ec8f959829a89
shankscript <shashsh2@cisco.com>
2018-07-31 19:49:56 +0530

    cleanup

--- build.js
+++ build.js
@@ -3,7 +3,7 @@ const { exec } = require('child_process');
 
 fs.emptyDirSync('./dist');
 
-exec(`emcc -o dist/ecms.html src/ecms.c -O3 -g4 -s WASM=1 --shell-file src/template.html -s NO_EXIT_RUNTIME=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' --source-map-base http://localhost:8888/`, (err, stdout, stderr) => {
+exec(`emcc -o dist/index.html src/ecms.c -O3 -g -s WASM=1 --shell-file src/template.html -s NO_EXIT_RUNTIME=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' --source-map-base http://localhost:8888/`, (err, stdout, stderr) => {
     if (err) {
         // node couldn't execute the command
         console.error('E!!!!!!!', err);
