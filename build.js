8d3266ce912b695d4e1ee7d4210662ec28367e2b
shankscript <shashsh2@cisco.com>
2018-07-31 19:36:43 +0530

    init

---
+++ build.js
@@ -0,0 +1,15 @@
+const fs = require('fs-extra');
+const { exec } = require('child_process');
+
+fs.emptyDirSync('./dist');
+
+exec(`emcc -o dist/ecms.html src/ecms.c -O3 -g4 -s WASM=1 --shell-file src/template.html -s NO_EXIT_RUNTIME=1 -s EXTRA_EXPORTED_RUNTIME_METHODS='["ccall","cwrap"]' --source-map-base http://localhost:8888/`, (err, stdout, stderr) => {
+    if (err) {
+        // node couldn't execute the command
+        console.error('E!!!!!!!', err);
+        return;
+    }
+    fs.copySync('./src', './dist/src');
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+});
